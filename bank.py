8b8a916641a6fb1e6a98f79d3430c589f627a3d0
L-Karimi <luciahkei101@gmail.com>
2022-05-27 21:50:35 +0300

    added functions in bank.py and student.py

--- bank.py
+++ bank.py
@@ -1,13 +1,22 @@
 from unicodedata import decomposition
 
-
 class Account:
     bank_name = "Equity"
     def __init__(self,account_number,account_balance) :
         self.account_number = account_number
         self.account_balance = account_balance
-    def deposit(self,account_balance):
-        self.account_balance = account_balance
-        
-    def withdraw(self,accunt_balance):
+    def deposit(self,amount):
+        self.amount+=amount
+        self.account_balance 
+        return self.balance
+        return f"my deposit is {amount} on the account {self.account_number} . The balance is {self.account_balance}"
+    
+    
+    def withdraw(self,amount):
+                self.amount+=amount
+
+                self.withdraw = amount
+                return self.amount
+                return f"my withdraw is {amount} on the account {self.account_number} . The balance is {self.account_balance}"
+
         
--- student.py
+++ student.py
@@ -1,9 +1,17 @@
 class Student:
     school = "AkiraChix"
-    def __init__(self,full_name,year_of_birth,initials) :
-        self.full_name = full_name
-        self.year_of_birth = year_of_birth
-        self.initials = initials
+    def __init__(self,first_name,last_name,age,origin_country) :
+        self.first_name = first_name
+        self.last_name = last_name
+        self.origin_country = origin_country
+        self.age = age
+    def full_name(self):
+       return f"Hello {self.first_name} {self.last_name}" 
+   
+    def year_of_birth(self):
+        return 2022-self.age
         
-    def full_name():
+    def initials(self):
+        return f"My initial are {self.first_name[0]} {self.last_name[0]}"
+        
         
