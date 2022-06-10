90636a787488580b78d27e51029b7ba32ea8b639
L-Karimi <luciahkei101@gmail.com>
2022-06-10 22:05:35 +0300

    modified bank.py

--- bank.py
+++ bank.py
@@ -1,22 +1,42 @@
-from unicodedata import decomposition
+from typing_extensions import Self
+from unicodedata import decomposition, name
 
 class Account:
     bank_name = "Equity"
-    def __init__(self,account_number,account_balance) :
+    def __init__(self,account_number,name) :
         self.account_number = account_number
-        self.account_balance = account_balance
-    def deposit(self,amount):
-        self.amount+=amount
-        self.account_balance 
-        return self.balance
-        return f"my deposit is {amount} on the account {self.account_number} . The balance is {self.account_balance}"
-    
-    
-    def withdraw(self,amount):
-                self.amount+=amount
-
-                self.withdraw = amount
-                return self.amount
-                return f"my withdraw is {amount} on the account {self.account_number} . The balance is {self.account_balance}"
-
+        self.account_balance = 0
+        self.name = name
+        self.deposits=[]
+        self.withdrawals=[]
         
+    def deposit(self,amount):
+
+        if amount<=0:
+            return amount>0
+        else:
+            self.account_balance+=amount
+        return f"my deposit is {self.deposits} on the account {self.account_number} . The balance is {self.account_balance}"
+
+    def withdraw(self,amount):
+            self.transaction=100
+
+            if amount>self.account_balance:
+              return f"Insufficient funds"
+            elif amount<=0:
+                return f"Insufficient funds"
+            else:
+                self.withdraw-=amount
+                return f"Hello{self.name}  you have withdrawn {self.withdrawals} and the balance is {self.account_balance} "
+            
+    def deposits_statement(self):
+        for x in self.desposits:
+            print(x,end="\n")
+    def withdrawals_statement(self):
+        for k in self.withdrawals:
+            print(k,end="\n")
+    def current_balance(self):
+        return f"Hello {Self.name} ,your current balance is {self.account_balance}"
+       
+            
+            
